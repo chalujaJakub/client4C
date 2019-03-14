c39dd5c9eea128dd00e67c22c7ac704ad801208b
katedra4c <katedra4c@sos-jh.cz>
2019-03-14 13:32:19 +0100

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function testovaciFunkce() {
+    var elem = document.getElementById("to");
+    
+    elem.style.color = "#ff0000";
+    elem.style.backgroundColor = "#0000ff";
+    //elem.style = "color: red; background-color: green;";
+    
+}
