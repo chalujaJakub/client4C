63ee61f6ef903b7b20a76b00af00a7b8538c670e
katedra4c <katedra4c@sos-jh.cz>
2019-03-14 14:50:53 +0100

    create user form support

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,27 @@ function testovaciFunkce() {
     //elem.style = "color: red; background-color: green;";
     
 }
+
+function submitFormWithUser() {
+    var xhr = new XMLHttpRequest();
+    var url = "http://localhost:8080/customer";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 201) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(json);
+        }
+    };
+    var data = JSON.stringify(
+        {
+            "name": getInputValue("form-name"), 
+            "city": getInputValue("form-city"), 
+            "grade": getInputValue("form-grade")
+        });
+    xhr.send(data);
+}
+
+function getInputValue(inputId) {
+    return document.getElementById(inputId).value;
+}
