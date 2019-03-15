e8976e545b1d3bf8fca1391294dd1ae11d5ab40e
katedra4c <katedra4c@sos-jh.cz>
2019-03-15 12:48:52 +0100

    user detail implemented

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+const SERVER_URL = "http://localhost:8080";
+
 function testovaciFunkce() {
     var elem = document.getElementById("to");
     
@@ -9,13 +11,13 @@ function testovaciFunkce() {
 
 function submitFormWithUser() {
     var xhr = new XMLHttpRequest();
-    var url = "http://localhost:8080/customer";
+    var url = SERVER_URL + "/customer";
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 201) {
             var json = JSON.parse(xhr.responseText);
-            console.log(json);
+            window.location.href = "user-detail.html?userId=" + json.id;
         }
     };
     var data = JSON.stringify(
@@ -27,6 +29,35 @@ function submitFormWithUser() {
     xhr.send(data);
 }
 
+function retrieveUserById() {
+    var userId = getUrlParameter("userId");
+    
+    var xhr = new XMLHttpRequest();
+    var url = SERVER_URL + "/customer/" + userId;
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            setInputValue("form-user-id", json.id);
+            setInputValue("form-name", json.name);
+            setInputValue("form-city", json.city);
+            setInputValue("form-grade", json.grade);
+        }
+    };
+    xhr.send();
+    
+}
+
+function getUrlParameter(parameterName) {
+    var url = new URL(document.URL);
+    return url.searchParams.get(parameterName);
+}
+
 function getInputValue(inputId) {
     return document.getElementById(inputId).value;
+}
+
+function setInputValue(inputId, inputValue) {
+    document.getElementById(inputId).value = inputValue;   
 }
